136a6637d7bce2b9a512d9ef0e605c8566bc820e
Hamza Aamer <50561910+hamzaaamer@users.noreply.github.com>
2023-04-19 16:38:31 +0500

    Fixed some mistakes

--- Table using Loop.cpp
+++ Table using Loop.cpp
@@ -4,12 +4,13 @@ int main()
 {
         int value1,value2;
         
-        cout<<"Enter the number of the table \n";
+        cout<<" Enter the number of the table ";
+        
         cin>>value1;
         
         cout<<endl;
         
-        for(value2 = 1; value2<=10; value2++){
+        for(value2 = 1; value2<=12; value2++){
         cout<<" "<< value1 <<" x "<< value2 <<" = "<< value1*value2<<endl;
         }
 
